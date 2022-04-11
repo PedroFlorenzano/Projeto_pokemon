d69dbe9baea0966e3a13151b9d6288b06cce4c29
Pedro Florenzano <pedroflorenzano.dev@gmail.com>
2022-04-11 19:30:46 -0300

    Add files via upload

---
+++ AC1-SIMULADOR DE BATALHAS/src/ac1.java
@@ -0,0 +1,30 @@
+public class ac1 {
+
+    public static void main(String[] args) {
+            /*
+            Pokemon pokemon = new Pokemon();// -- dá erro pq obriga a qualquer pessoa que iniciar um pokemon a passar os atributos dele
+            pokemon.vida = 1000;  //-- isso não pode ocorrer, temos em encapsular para não ser mudado
+            double defesa;
+            
+            Pokemon onyx = new Pokemon ("Pedrinha","Terra",50);//aqui só passou os atributos obrigatórios que estão no construtor
+            defesa = onyx.getDefesa();
+            */
+        Pokemon meuPokemon = new Pokemon("Charmander","Fogo",30);
+        
+        Pokemon outroPokemon = new Pokemon("Blatoise","Agua",30);
+            
+        Batalhas batalha1 = new Batalhas();
+               
+        System.out.println("Seu Pokémon: ");
+        meuPokemon.imprimiPokemon();
+        System.out.println("Pokémon Inimigo: ");
+        outroPokemon.imprimiPokemon();
+        
+        batalha1.meuPokemon(meuPokemon.getNome(), (int) meuPokemon.getVida());
+        batalha1.outroPokemon(outroPokemon.getNome(), (int) outroPokemon.getVida());
+        
+        batalha1.startBattle();               
+    }
+
+}
+    
